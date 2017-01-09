31af8cf58a1b8b8cd396ba03f2a2f1984a1bbe10
piotrantas <antas.piotr@gmail.com>
2017-01-09 21:54:44 +0100

    corrected syntax

--- scripts.js
+++ scripts.js
@@ -1,37 +1,27 @@
 // plik scripts.js
 
-var a = prompt ("podaj wartość a"),
-var b = prompt('podaj wartość b'),
-var wynik = a*a + 2 * a * b - b*b
+var a = prompt("podaj wartość a");
+var b = prompt('podaj wartość b');
+var wynik = a * a + 2 * a * b - b * b;
 
 console.log(wynik);
 
 if (wynik > 0) {
-	console.log('dodatni')
-} 
-else
-{
-	console.log('ujemny')
+	console.log('dodatni');
+} else {
+	console.log('ujemny');
 }
-
 switch (wynik) {
 	case 0:
 		console.log('wynik równy zero');
 		break;
 	default:
-		console.log('wynik rózny od zera')
+		console.log('wynik rózny od zera');
 }
-
-
-/*
 if (wynik > 0) {
-	console.log('dodatni')
-} 
-else if (wynik < 0) 
-{
+	console.log('dodatni');
+} else if (wynik < 0) {
 	console.log('ujemny')
-}
-else {
+} else {
 	console.log ('wynik jest 0')
 }
-*/
