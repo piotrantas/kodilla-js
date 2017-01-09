ac88dc5466f83df4589427e5be2a6689e4d7bfb0
piotrantas <antas.piotr@gmail.com>
2017-01-09 22:16:14 +0100

    symmetrical tree

--- scripts.js
+++ scripts.js
@@ -2,12 +2,17 @@
 
 
 function rysuj(p) {
-for ( var i = 1 ; i <= p ; i++) {
-  var star = ' ';
-  for (var n = 1; n <= i * 2 - 1; n++) {
-  	star += ' *';
-  }
-  console.log(star);
+for ( var i = 1 ; i <= p ; i++) { 
+	var star = ''; 
+	for (var n = 1; n <= i * 2 - 1; n++) { 
+	star += '*'; 
+}
+	var space = ''; 
+	for (var k = 1; k <= p - i; k++) {
+	space += ' ';
+}
+console.log(space + star); 
 }
 }
-rysuj(3)
+
+rysuj(10)
